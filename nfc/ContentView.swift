aa3e319bcc29c67afad91246ce5fcd5c73600da5
Emmanuel Korir <korirm3@gmail.com>
2023-05-31 17:35:21 +0300

    IOS init

--- nfc/ContentView.swift
+++ nfc/ContentView.swift
@@ -6,21 +6,48 @@
 //
 
 import SwiftUI
+import CoreNFC
 
 struct ContentView: View {
+    @State var urlT = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+            TextField("Enter your name", text: $urlT)
+            Button(action:{}){
+                Text("Scan card")
+            }.padding()
         }
-        .padding()
+      
+//        .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }nfc
+    
+}
+
+
+class NFCReader: NSObject, ObservableObject, NFCNDEFReaderSessionDelegate{
+    
+    var theactualData = ""
+    var nfcSession: NFCNDEFReaderSession?
+    
+    func scan(theactualData: String){
+        self.theactualData = theactualData
+        nfcSession = NFCNDEFReaderSession(delegate: self, queue: nil, invalidateAfterFirstRead: true)
     }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didInvalidateWithError error: Error) {
+        <#code#>
+    }
+    
+    func readerSession(_ session: NFCNDEFReaderSession, didDetectNDEFs messages: [NFCNDEFMessage]) {
+        <#code#>
+    }
+    
+
 }
