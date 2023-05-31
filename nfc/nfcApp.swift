f92671563014bc4b62682787c1c4a5b6074561e1
Emmanuel Korir <korirm3@gmail.com>
2023-05-31 15:23:58 +0300

    Initial Commit

---
+++ nfc/nfcApp.swift
@@ -0,0 +1,17 @@
+//
+//  nfcApp.swift
+//  nfc
+//
+//  Created by Korir on 31/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct nfcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
